1d51cbe0f3be520272f2935f47d37755e1523be6
karrde <karrde@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-20 04:44:24 +0000

    rewrote with wp2.8+ new widget interface code, fixed tag feed url (was retrieving an atom feed instead of rss), moved feed retrieval to the new Wordpress default parser (SimplePie)
    git-svn-id: https://plugins.svn.wordpress.org/flickr-thumbnails-widget/trunk@324453 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickrrss.php
+++ flickrrss.php
@@ -1,117 +1,114 @@
 <?php
+/**
+* @package FlickrThumbsWidget
+* @version 1.5
+*/
 /*
 Plugin Name: Flickr Thumbnails Widget
+Plugin URI: http://www.karrderized.com/old-stuff/flickr-thumbnail-widget/
 Description: Displays thumbnails from Flickr based on the user/group/tag you provide.
 Author: James Carppe
-Version: 1.3
-Author URI: http://www.karrderized.com/old-stuff/flickr-thumbnail-widget/
+Version: 1.5
+Author URI: http://www.karrderized.com/
 */
 
 // Changelog:
+// v1.5: fixed tag feed url (was retrieving an atom feed instead of rss), moved feed retrieval to the new Wordpress default parser (SimplePie)
+// v1.4: rewrote with wp2.8+ new widget interface code
 // v1.3: added column support (thanks debashish!)
 // v1.2: .. too long ago to remember ;)
 
-function widget_flickrthumbs_init() {
 
-	if ( !function_exists('register_sidebar_widget') )
-		return;
+add_action('widgets_init', 'flickrthumbs_load_widgets');
 
-	function widget_flickrthumbs($args) {
-		
-		extract($args);
+function flickrthumbs_load_widgets() {
+  register_widget('FlickrThumbsWidget');
+}
 
-		$options = get_option('widget_flickrthumbs');
-		$title = $options['title'];
-		$searchtext = $options['searchtext'];
-		$type = $options['type'];
-		$numpics = $options['numpics'];
-		$numcols = $options['numcols'];
-		
-		echo $before_widget . $before_title . $title . $after_title;
-		if ($type == "group") {
+class FlickrThumbsWidget extends WP_Widget {
+  function FlickrThumbsWidget() {
+    $widget_ops = array('classname'=>'flickrthumbs', 'description'=>'Displays thumbnails from Flickr based on the user/group/tag you provide.');
+    $control_ops = array('width'=>300, 'height'=>150, 'id_base'=>'flickrthumbs-widget');
+    $this->WP_Widget('flickrthumbs-widget', 'Flickr Thumbnails', $widget_ops, $control_ops);
+  }
+  
+  function widget($args, $instance) {
+    extract($args);
+    $title = apply_filters('widget_title', $instance['title']);
+    $searchtext = $instance['searchtext'];
+    $type = $instance['type'];
+    $numpics = $instance['numpics'];
+    $numcols = $instance['numcols'];
+    
+    echo $before_widget . $before_title . $title . $after_title;
+    if ($type == "group") {
 			$feedurl = "http://www.flickr.com/groups/" . $searchtext . "/pool/feed/?format=rss_200";
 		} else if ($type == "tag") {
-			$feedurl = "http://www.flickr.com/services/feeds/photos_public.gne?tags=" . $searchtext;
+			$feedurl = "http://www.flickr.com/services/feeds/photos_public.gne?tags=" . $searchtext . "&format=rss_200";
 		} else {
 			$feedurl = "http://www.flickr.com/services/feeds/photos_public.gne?id=" . $searchtext . "&format=rss_200";
 		}
-		require_once (ABSPATH . WPINC . '/rss-functions.php');
-		$rss = fetch_rss($feedurl);
-		define('MAGPIE_CACHE_AGE', 300);
-
-		if ( $rss ) {
+		require_once (ABSPATH . WPINC . '/feed.php');
+		$rss = fetch_feed($feedurl);
+		
+    if (!is_wp_error( $rss ) ) { // Checks that the object is created correctly 
+      $maxitems = $rss->get_item_quantity($numpics);
+      $rss_items = $rss->get_items(0, $maxitems);
 			$count = 0;			
 			echo "<table border=0 width=100%><tr>";
-			foreach ($rss->items as $item ) {
-				if ($count < $numpics) {
-				    $r = $count % $numcols;				    
-					if($r == 0) {
-						echo "<tr>";
-					}					
-					preg_match('<img src="([^"]*)" [^/]*/>', $item['description'],$imgUrlMatches);
-					$imgurl = $imgUrlMatches[1];
-					$imgurl = str_replace("m.jpg", "s.jpg", $imgurl);
-					$thetitle = $item[title];
-					$thelink = $item[link];
-					echo "<td><a href=\"$thelink\" title=\"$thetitle\"><img width=\"75px\" height=\"75px\" src=\"$imgurl\" alt=\"$thetitle\" /></a></td>";
-					if($r == ($numcols - 1)) {
-						echo "</tr>";
-					}
-					$count++;										
+			foreach ($rss_items as $item ) {
+				$r = $count % $numcols;				    
+				if($r == 0) {
+					echo "<tr>";
+				}					
+				preg_match('<img src="([^"]*)" [^/]*/>', $item->get_description(),$imgUrlMatches);
+				$imgurl = $imgUrlMatches[1];
+				$imgurl = str_replace("m.jpg", "s.jpg", $imgurl);
+				$thetitle = $item->get_title();
+				$thelink = $item->get_permalink();
+				echo "<td><a href=\"$thelink\" title=\"$thetitle\"><img width=\"75px\" height=\"75px\" src=\"$imgurl\" alt=\"$thetitle\" /></a></td>";
+				if($r == ($numcols - 1)) {
+					echo "</tr>";
 				}
+				$count++;										
 			}
 			echo "</table>";
-		}
-		else {
+		} else {
 		    echo "An error occured!  " .      
-		        "<br>Error Message: "   . magpie_error();
+		        "<br>Error Message: "   . wp_error();
 		}
 		
-		echo $after_widget;
-	}
-
-	function widget_flickrthumbs_control() {
-
-		$options = get_option('widget_flickrthumbs');
-		if ( !is_array($options) )
-			$options = array('title'=>'Flickr', 'searchtext'=>'', 'type'=>'user', 'numpics'=>'6', 'numcols'=>'3');
-		if ( $_POST['flickrthumbs-submit'] ) {
-
-			$options['title'] = strip_tags(stripslashes($_POST['flickrthumbs-title']));
-			$options['searchtext'] = strip_tags(stripslashes($_POST['flickrthumbs-searchtext']));			
-			$options['type'] = strip_tags(stripslashes($_POST['flickrthumbs-type']));
-			$options['numpics'] = strip_tags(stripslashes($_POST['flickrthumbs-numpics']));			
-			$options['numcols'] = strip_tags(stripslashes($_POST['flickrthumbs-numcols']));	
-			update_option('widget_flickrthumbs', $options);
-		}
-
-		$title = htmlspecialchars($options['title'], ENT_QUOTES);
-		$searchtext = htmlspecialchars($options['searchtext'], ENT_QUOTES);
-		$type = htmlspecialchars($options['type'], ENT_QUOTES);
-		$numpics = htmlspecialchars($options['numpics'], ENT_QUOTES);
-		$numcols = htmlspecialchars($options['numcols'], ENT_QUOTES);
+		echo $after_widget;    
+  }
+  
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    
+    $instance['title'] = strip_tags($new_instance['title']);
+    $instance['searchtext'] = strip_tags($new_instance['searchtext']);
+    $instance['type'] = strip_tags($new_instance['type']);
+    $instance['numpics'] = strip_tags($new_instance['numpics']);
+    $instance['numcols'] = strip_tags($new_instance['numcols']);
+    
+    return $instance;    
+  }
+  
+  function form($instance) {
+    $defaults = array('title'=>'Flickr', 'searchtext'=>'', 'type'=>'group', 'numpics'=>10, 'numcols'=>1);
+    $instance = wp_parse_args((array) $instance, $defaults); ?>
+    
+    <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input style="width: 200px;" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('searchtext'); ?>">Search Text: <input style="width: 200px;" id="<?php echo $this->get_field_id('searchtext'); ?>" name="<?php echo $this->get_field_name('searchtext'); ?>" type="text" value="<?php echo $instance['searchtext']; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('type'); ?>">Type:
+    <input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="group"<?php if($instance['type'] == 'group') {echo ' checked';} ?>>group</input>
+		<input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="tag"<?php if($instance['type'] == 'tag') {echo ' checked';} ?>>tag</input>
+		<input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="user"<?php if($instance['type'] == 'user') {echo 'checked';} ?>>user</input>
+    </label></p>
+		<p><label for="<?php echo $this->get_field_id('numpics'); ?>">Number of images to show: <input style="width: 50px;" id="<?php echo $this->get_field_id('numpics'); ?>" name="<?php echo $this->get_field_name('numpics'); ?>" type="text" value="<?php echo $instance['numpics']; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('numcols'); ?>">Number of columns: <input style="width: 50px;" id="<?php echo $this->get_field_id('numcols'); ?>" name="<?php echo $this->get_field_name('numcols'); ?>" type="text" value="<?php echo $instance['numcols']; ?>" /></label></p>
 		
-		echo '<p style="text-align:right;"><label for="flickrthumbs-title">Title: <input style="width: 200px;" id="flickrthumbs-title" name="flickrthumbs-title" type="text" value="'.$title.'" /></label></p>';
-		echo '<p style="text-align:right;"><label for="flickrthumbs-searchtext">Search Text: <input style="width: 200px;" id="flickrthumbs-searchtext" name="flickrthumbs-searchtext" type="text" value="'.$searchtext.'" /></label></p>';
-		echo '<p style="text-align:right;"><label for="flickrthumbs">Type: <input type="radio" name="flickrthumbs-type" value="group"';
-		if($type == 'group') {echo 'checked';}
-		echo '>group</input> ';
-		echo '<input type="radio" name="flickrthumbs-type" value="tag"';
-		if($type == 'tag') {echo 'checked';}
-		echo '>tag</input> ';
-		echo '<input type="radio" name="flickrthumbs-type" value="user"';
-		if($type == 'user') {echo 'checked';}
-		echo '>user</input></label></p>';
-		echo '<p style="text-align:right;"><label for="flickrthumbs-numpics">Number of images to show: <input style="width: 50px;" id="flickrthumbs-numpics" name="flickrthumbs-numpics" type="text" value="'.$numpics.'" /></label></p>';
-		echo '<p style="text-align:right;"><label for="flickrthumbs-numpics">Number of columns: <input style="width: 50px;" id="flickrthumbs-numcols" name="flickrthumbs-numcols" type="text" value="'.$numcols.'" /></label></p>';
-		echo '<input type="hidden" id="flickrthumbs-submit" name="flickrthumbs-submit" value="1" />';
-	}
-	
-	register_sidebar_widget('Flickr Thumbnails', 'widget_flickrthumbs');
-
-	register_widget_control('Flickr Thumbnails', 'widget_flickrthumbs_control', 300, 150);
+		<?php
+  }
 }
-
-add_action('plugins_loaded', 'widget_flickrthumbs_init');
 
 ?>
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: karrde
 Donate link: http://www.karrderized.com/old-stuff/flickr-thumbnails-widget/
 Tags: flickr, widget, thumbnails, rss
-Requires at least: 2.0.2
-Tested up to: 3.02
+Requires at least: 2.8
+Tested up to: 3.03
 Stable tag: trunk
 
 Displays thumbnails from Flickr based on the user/group/tag you provide.
@@ -14,11 +14,17 @@ Displays thumbnails from Flickr based on the user/group/tag you provide.
 
 == Installation ==
 
-1. Upload `flickrrss.php` to the `/wp-content/plugins/` directory
+1. Upload the contents of the zip file to your Wordpress install's plugin directory (usually `/wp-content/plugins/`)
 1. Activate the plugin through the 'Plugins' menu in WordPress
 1. Add the widget to your bar through the Widgets page and configure it to your liking.
 
 == Changelog ==
+
+= 1.5 =
+* fixed tag feed url (was retrieving an atom feed instead of rss), moved feed retrieval to the new Wordpress default parser (SimplePie)
+
+= 1.4 =
+* rewrote with wp2.8+ new widget interface code
 
 = 1.3 =
 * added column support (thanks [debashish](http://www.debashish.com/)!)
